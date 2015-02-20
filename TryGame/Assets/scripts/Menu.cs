a18c1382e0a8e94822353be8ba0accbdefd9ff6d
alkalinnne <rogeriocsilva21@gmail.com>
2015-02-20 03:09:52 +0000

    yfu

---
+++ TryGame/Assets/scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height / 3, Screen.width / 5, Screen.height / 4),"Start")) {
+			Application.LoadLevel(1);	
+		
+		}
+	}
+}
