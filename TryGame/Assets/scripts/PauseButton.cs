ee39f0a7c89fd9366f691baa60c9d2114cb13db5
alkalinnne <rogeriocsilva21@gmail.com>
2015-02-20 03:58:13 +0000

    V2.1.2

---
+++ TryGame/Assets/scripts/PauseButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseButton : MonoBehaviour {
+
+	bool isPaused;
+	
+	void Start()
+	{
+		isPaused = false;
+	}
+	
+	void OnGUI()
+	{ 
+		if (!isPaused)
+		{
+			if (GUI.Button(new Rect(10,10,50,50), "Pause"))
+			{
+				Time.timeScale = 0f;
+				isPaused = true;
+			}
+		}
+		if (isPaused)
+		{
+			if (GUI.Button(new Rect(10,10,50,50), "Play"))
+			{
+				Time.timeScale = 1.0f;
+				isPaused = false;
+			}
+		}
+	}
+}
