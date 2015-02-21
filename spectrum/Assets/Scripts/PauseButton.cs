2aa10b9861ddca7e051ab427ed127c82d85425c1
João Oliveira <jpoliveira11@gmail.com>
2015-02-21 16:06:56 +0000

    V2.6
    menus

---
+++ spectrum/Assets/Scripts/PauseButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseButton : MonoBehaviour {
+	
+	public void pauseSystem(){
+		Time.timeScale = 0f;
+	}
+	public void resumeSystem(){
+		Time.timeScale = 1f;
+	}
+}
